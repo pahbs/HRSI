07bfe6bad72d315473fa3c3e191e86637b3945f2
mwooten3 <margaret.wooten@nasa.gov>
2018-09-27 16:47:33 -0400

    edits to iterate script

--- iterateDir_runZonalStats.py
+++ iterateDir_runZonalStats.py
@@ -4,16 +4,17 @@ import os, sys
 import glob
 
 runScript = '/att/home/mwooten3/code/HRSI/run_GLAS_zonal_database.py'
-#indir = '/att/gpfsfs/briskfs01/ppl/wcwagne1/3DSI/hrsi_chms/Stacks_20180717/'
-indir = '/att/gpfsfs/briskfs01/ppl/wcwagne1/3DSI/hrsi_chms/Stacks_20180713/' #TEMP
+indir = '/att/gpfsfs/briskfs01/ppl/wcwagne1/3DSI/hrsi_chms/Ontario/'
 # indir is set up like this: indir/<pairname>/stack.tif'
 globDir = os.path.join(indir, '*', '*stack.tif')
 
-#outdir_base = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS_zonal/Stacks_20180717'
-outdir_base = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS_zonal/Stacks_20180713' # TEMP
+outdir_base = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS_zonal/Stacks_Ontario'
 outDir = os.path.join(outdir_base, 'outputs')
 shpDir = os.path.join(outdir_base, 'shp')
 logDir = os.path.join(outdir_base, 'logs')
+
+# set mainDb = '' if you want to use the default
+mainDb = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS_zonal/Stacks_Ontario/Ontario_Stacks__zonalStats_15m.csv'
 
 for d in [outDir, shpDir, logDir]:
     os.system('mkdir -p {}'.format(d))
@@ -28,5 +29,8 @@ for pairStack in glob.glob(globDir):
 ##        continue
     comm = 'python {} {} -shpDir {} -outDir {} -logDir {}'.format(runScript, pairStack, shpDir, outDir, logDir)
 ##    comm = 'python {} {} -shpDir {} -outDir {} -logDir {} -mainDatabasePrefix /att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/3DSI_GLAS_stats_database_15m__p2.csv'.format(runScript, pairStack, shpDir, outDir, logDir) # TEMP
+    if mainDb:
+        comm += ' -mainDatabasePrefix {}'.format(mainDb)
+
     print comm
     os.system(comm)
