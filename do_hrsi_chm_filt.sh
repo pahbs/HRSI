e5a5fd3c8a8c24d1c934be268f24a017a52791c7
Paul Montesano <paul.montesano@gmail.com>
2019-09-27 15:35:43 -0400

    simple code updates

--- do_hrsi_chm_filt.sh
+++ do_hrsi_chm_filt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # 
-# Multi-res Filtering of ASP Point Clouds for estimating Forest Canopy Heights
+# Filtering of ASP Point Clouds for estimating Forest Canopy Heights from HRSI stereopairs
 #  
-# formerly "test_point2dem.sh"
+# see "run_do_hrsi_chm_filt.sh" for 3DSI batch runs
 # 
 # Example call:
-# pupsh "hostname ~ 'ecotone05'" "do_dem_filt-TEST2.sh WV01_20110912_10200100157A5A00_1020010015E8D800 1 4 5 2 true true true true"
+# pupsh "hostname ~ 'ecotone05'" "do_hrsi_chm_filt.sh WV01_20150505_102001003DB7FB00_102001003D03CA00 1 4 10 2 true true true"
 # or
-# pmontesa@ecotone05:~$ do_dem_filt-TEST2.sh WV01_20110912_10200100157A5A00_1020010015E8D800 1 4 5 2 true true true true
+# pmontesa@ecotone05:~$ do_dem_filt-TEST2.sh WV01_20110912_10200100157A5A00_1020010015E8D800 1 4 5 2 true true true
 
 pairname=$1
 res_fine=$2			# the fine pixel res for DEMs
@@ -25,61 +25,83 @@ search_rad=$5		# Search radius (# pixels) used for point2dem filtering
 #Format for output file naming
 search_rad_frmt=$(echo $search_rad | awk '{printf("%02d", $1)}')
 
-PUBREPO=$6		#Input data in /att/pubrepo/DEM/hrsi_dsm ? 
+INPUT=$6		#Input data in /att/pubrepo/DEM/hrsi_dsm ? 
 DO_P2D=$7		#Do the point2dem block of this script?
 DO_DZ=$8		#Do the masking & differencing block?
 
-p2d_extent="${9}"
+p2d_extent=${9:-''}
 
-# If PUBREPO is false
+# If INPUT is not either v1 or v2 this will be used
 batch_name=${10}
 
 # Percentage by which the resolution of the DEM is reduced to produce a slope raster
 reduce_pct_slope=100
 
-# Output res for dz files
-out_dz_res=2
+out_dz_res=2 
 
-#-------------------------------------------------------------------------------------
+echo; echo "Script call:"
+echo "${0} ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10}" ; echo
+echo "Processing CHMs from pairname: $pairname"
+echo "Processing parameters for point-cloud filtering"
+echo "Pixel res. for local max: $res_fine"
+echo "Pixel res. for local min: $res_coarse"
+echo "Search radius (in pixels): $search_rad"
+echo "Max slope above which pixels will be masked out: $max_slope"
+echo "Output res. for CHM files: $out_dz_res"
 
-if [ "${PUBREPO}" = true ] ; then
+if [ "${INPUT}" = "v1" ] || [ "${INPUT}" = "true" ] ; then
     main_dir=/att/pubrepo/DEM/hrsi_dsm
+    work_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/outASP_TEST/test2_do_dem_filt
+elif [ "${INPUT}" = "v2" ] ; then
+    main_dir=/att/pubrepo/DEM/hrsi_dsm/v2
+    work_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/chm_work/hrsi_chm_sgm_filt
+elif [ "${INPUT}" = "test" ] ; then
+    main_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/outASP/veg_mode/SGM_5_999_0
+    work_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/chm_work/hrsi_chm_sgm_filt
 else
     main_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/outASP/${batch_name}
+    work_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/chm_work/${batch_name}
 fi
 
-echo; echo "Main dir: ${main_dir}"; echo
+pairname_dir=$work_dir/$pairname
 
-workdir=/att/gpfsfs/briskfs01/ppl/pmontesa/outASP_TEST/test2_do_dem_filt/${pairname}
-workdir_old=/att/gpfsfs/briskfs01/ppl/pmontesa/outASP_TEST/test_do_dem_filt/${pairname}
-mkdir -p $workdir
+echo "Main dir: ${main_dir}"
+echo "Work dir: ${work_dir}"
 
-if [ -e ${main_dir}/${pairname}/out-strip-PC.tif ] ; then
-    ln -sf ${main_dir}/${pairname}/out-strip-PC.tif $workdir/out-PC.tif
-else
-    ln -sf ${main_dir}/${pairname}/out-PC.tif $workdir/out-PC.tif
+mkdir -p $work_dir
+mkdir -p ${work_dir}/chm
+
+# This is important so you dont overwrite the original PC.tif when you are writing the CHMs to the same dir as the input...
+if [ "${INPUT}" = "v1" ] || [ "${INPUT}" = "v2" ] || [ "${INPUT}" = "test" ]; then
+
+    if [ -e ${main_dir}/${pairname}/out-strip-PC.tif ] ; then
+        ln -sf ${main_dir}/${pairname}/out-strip-PC.tif $pairname_dir/out-PC.tif 2> /dev/null
+    else
+        ln -sf ${main_dir}/${pairname}/out-PC.tif $pairname_dir/out-PC.tif 2> /dev/null
+    fi
+
+    # Make symlinks to original data needed
+    ln -sf ${main_dir}/${pairname}/out-DEM_4m.tif $pairname_dir/out-DEM_4m.tif 2> /dev/null
+    ln -sf ${main_dir}/${pairname}/out-DEM_24m.tif $pairname_dirr/out-DEM_24m.tif 2> /dev/null
+    ln -sf ${main_dir}/${pairname}/${pairname}_ortho.tif $pairname_dir/${pairname}_ortho.tif 2> /dev/null
 fi
 
-# Make symlinks to original data needed
-ln -sf ${main_dir}/${pairname}/out-DEM_4m.tif $workdir/out-DEM_4m.tif
-ln -sf ${main_dir}/${pairname}/out-DEM_24m.tif $workdir/out-DEM_24m.tif
-ln -sf ${main_dir}/${pairname}/${pairname}_ortho.tif $workdir/${pairname}_ortho.tif
-
-proj=$(utm_proj_select.py ${workdir}/out-DEM_24m.tif)
-echo $proj
+proj=$(utm_proj_select.py ${pairname_dir}/out-DEM_24m.tif)
+echo; echo "Projection: $proj" ; echo
 
 if [ "${proj}" = "" ] || [ -e "${proj}" ] ; then
-    echo "Proj string empty: ${workdir}/out-DEM_24m.tif . Exiting."
+    echo; echo "Proj string empty: ${pairname_dir}/out-DEM_24m.tif . Exiting." ; echo
     exit 1
 fi
 
 p2d_opts=''
 p2d_opts="--t_srs \"$proj\""
 if [ -z "$p2d_extent" ] ; then
-    echo "No projwin."
+    echo "No specified subset window for output CHM." ; echo
 else
     p2d_opts+=" --t_projwin $p2d_extent"
 fi
+
 p2d_opts+=" --remove-outliers --remove-outliers-params 75.0 3.0"
 p2d_opts+=" --threads 4 --nodata-value -99"
 p2d_opts+=" --search-radius-factor $search_rad"
@@ -97,33 +119,46 @@ if [ "${DO_P2D}" = true ]; then
             res=${res_fine}
         fi
         
-        if [ -e $workdir/out_${res}m-sr${search_rad_frmt}-${filt}-DEM.tif ] ; then
+        if [ -e $pairname_dir/out_${res}m-sr${search_rad_frmt}-${filt}-DEM.tif ] && [ -e $pairname_dir/out_${res}m-sr${search_rad_frmt}-DEM_complete ]  ; then
             echo "DEM exists: out_${res}m-sr${search_rad_frmt}-${filt}-DEM.tif"
         else
             echo "Get the $filt DEM at ${res}m using search radius ${search_rad}" 
-            cmd+="point2dem --filter $filt $p2d_opts --tr $res -o $workdir/out_${res}m-sr${search_rad_frmt} $workdir/out-PC.tif ; "
-            echo $cmd
+            cmd+="point2dem --filter $filt $p2d_opts --tr $res -o $pairname_dir/out_${res}m-sr${search_rad_frmt} $pairname_dir/out-PC.tif && touch $work_dir/out_${res}m-sr${search_rad_frmt}-DEM_complete ; "
+            echo; echo $cmd ; echo
             cmd_list+=\ \'$cmd\'
         fi
     done
     if [ -z "$cmd_list" ] ; then
-        echo "DEMs already exist."
+        echo "DEMs already exists."
     else
         eval parallel -j 4 ::: $cmd_list
     fi
 fi
 
-if [ "${DO_DZ}" = true ] ; then
 
-    echo; echo "Get a slope masks from 2 resolutions..."; echo
+# Set intermediate tail; ground (min) and canopy (max) DSMs; and out_dz file name
+tail="-DEM_masked_masked"
+min_dsm=$pairname_dir/out_${res_coarse}m-sr${search_rad_frmt}-min${tail}.tif
+max_dsm=$pairname_dir/out_${res_fine}m-sr${search_rad_frmt}-max${tail}.tif
+out_dz_tmp=${min_dsm%.*}_$(basename ${max_dsm%.*})_dz_eul.tif
+out_dz=${pairname_dir}/${pairname}_sr${search_rad_frmt}_$(echo $(basename ${out_dz_tmp}) | sed -e 's/out_//g' | sed -e "s/${tail}//g" )
+
+if [ -e "$out_dz" ] ; then
+    echo; echo "CHM already exists:" ; echo ${out_dz} ; echo
+elif [[ "${DO_DZ}" = false ]] && [[ ! -e "$out_dz" ]] ; then
+    echo; echo "CHM DOESNT exists, and you chose not to compute it." ; echo
+elif [[ "${DO_DZ}" = true ]] && [[ ! -e "$out_dz" ]] ; then
+    echo; echo "Computing CHM:" ; echo ${out_dz}
+    echo "Get a slope masks from 2 resolutions..."; echo
+
     rp_multi=100
     cmd_list=""
-    if [ ! -e "$workdir/out-DEM_24m_slopemasked.tif" ] ; then
-        cmd="slopemask_dem.py $workdir/out-DEM_24m.tif -max_slope ${max_slope} ; "
+    if [ ! -e "$pairname_dir/out-DEM_24m_slopemasked.tif"  ] ; then 
+        cmd="slopemask_dem.py $pairname_dir/out-DEM_24m.tif -max_slope ${max_slope} ; "
         cmd_list+=\ \'$cmd\'
     fi
-    if [ ! -e "$workdir/out-DEM_4m_slopemasked.tif" ] ; then
-        cmd="slopemask_dem.py $workdir/out-DEM_4m.tif -max_slope ${max_slope} ; "
+    if [ ! -e "$pairname_dir/out-DEM_4m_slopemasked.tif"  ] ; then
+        cmd="slopemask_dem.py $pairname_dir/out-DEM_4m.tif -max_slope ${max_slope} ; "
         cmd_list+=\ \'$cmd\'
     fi
     if [ -z "$cmd_list" ] ; then
@@ -131,66 +166,62 @@ if [ "${DO_DZ}" = true ] ; then
     else
         eval parallel -j 2 ::: $cmd_list
     fi
-
-    # Set intermediate tail; ground (min) and canopy (max) DSMs; and out_dz file name
-    tail="-DEM_masked_masked"
-    min_dsm=$workdir/out_${res_coarse}m-sr${search_rad_frmt}-min${tail}.tif
-    max_dsm=$workdir/out_${res_fine}m-sr${search_rad_frmt}-max${tail}.tif
-    out_dz_tmp=${min_dsm%.*}_$(basename ${max_dsm%.*})_dz_eul.tif
-    out_dz=${workdir}/${pairname}_sr${search_rad_frmt}_$(echo $(basename ${out_dz_tmp}) | sed -e 's/out_//g' | sed -e "s/${tail}//g" )
     
     echo; echo $(basename ${out_dz}) ; echo
-    if [ ! -e "$out_dz" ] ; then
 
-        # Apply slope masks
-        cmd_list=''
 
-        for filt in $filt_list ; do 
-            for res in $res_list ; do
-                cmd=''
-                dsm=$workdir/out_${res}m-sr${search_rad_frmt}-${filt}-DEM.tif   
-                if [ -e "$dsm" ] && [ ! -e "${dsm%.*}_masked_masked.tif" ] ; then
-                    echo "Apply slope masks from 2 resolutions..."
-                    # These two must be in serial
-                    cmd+="apply_mask_pm.py -extent intersection ${dsm} $workdir/out-DEM_24m_slopemasked.tif ; "
-                    cmd+="apply_mask_pm.py -extent intersection ${dsm%.*}_masked.tif $workdir/out-DEM_4m_slopemasked.tif ; "
-                    echo; echo $cmd ; echo
-                    cmd_list+=\ \'$cmd\'  
+    # Apply slope masks
+    cmd_list=''
+
+    for res in $res_list ; do  
+        for filt in $filt_list ; do
+
+            cmd=''
+            dsm=$pairname_dir/out_${res}m-sr${search_rad_frmt}-${filt}-DEM.tif
+   
+            if [ -e "$dsm" ] ; then
+                echo "Apply slope masks from 2 resolutions..."
+                echo "DSM: $dsm"
+                # These two must be in serial
+                if [[ ( ! -e "${dsm%.*}_masked_complete" ) || ( ! -e  "${dsm%.*}_masked.tif" ) ]] ; then
+                    echo "Mask: out-DEM_24m_slopemasked.tif"
+                    cmd+="apply_mask_pm.py -extent intersection ${dsm} $pairname_dir/out-DEM_24m_slopemasked.tif && touch ${dsm%.*}_masked_complete ; "
                 fi
+                if [[ ( ! -e "${dsm%.*}_masked_masked_complete" ) || ( ! -e  "${dsm%.*}_masked_masked.tif" ) ]] ; then
+                    echo "Mask: out-DEM_4m_slopemasked.tif"
+                    cmd+="apply_mask_pm.py -extent intersection ${dsm%.*}_masked.tif $pairname_dir/out-DEM_4m_slopemasked.tif && touch ${dsm%.*}_masked_masked_complete ; "
+                fi
+                echo; echo $cmd ; echo
+                cmd_list+=\ \'$cmd\'  
+            fi
                 
-            done   
-        done
+        done   
+    done
+    # KEEP JOBS at 2!!
+    eval parallel -j 2 ::: $cmd_list
+    
+    echo; echo "Min dsm, Max dsm: $(basename $min_dsm) , $(basename $max_dsm)"
+    echo $(basename $out_dz)
+             
+    compute_dz.py -tr $out_dz_res $min_dsm $max_dsm
+    mv ${out_dz_tmp} ${out_dz}
+    gdaladdo -ro -r average ${out_dz} 2 4 8 16 32 64
 
-        eval parallel -j 2 ::: $cmd_list
-        
-        cmd_list=''   
-
-        #for min_dsm in $workdir/out_${res_coarse}m-sr${search_rad_frmt}-min${tail}.tif; do
-        #for max_dsm in $workdir/out_${res_fine}m-sr${search_rad_frmt}-max${tail}.tif ; do                
-                
-                echo; echo "Min dsm, Max dsm: $(basename $min_dsm) , $(basename $max_dsm)"
-                echo $(basename $out_dz)
-                
-                compute_dz.py -tr $out_dz_res $min_dsm $max_dsm
-                mv ${out_dz_tmp} ${out_dz}
-                gdaladdo -ro -r average ${out_dz} 2 4 8 16 32 64
-
-        #done
-        #done
+    if [ -e "$out_dz" ] ; then
+        echo; echo "CHM finished:" ; echo ${out_dz} ; echo
+    else
+        echo; echo "Fail: CHM not made for some reason." ; echo
+        echo $pairname >> ${work_dir}/list_fails
     fi
+
 fi
-
-echo; echo "Done with ${pairname}" ; echo
-
+ 
 # Make links to completed dZ tifs in a top level 'chm' dir for easy access
-for dz in `ls ${workdir}/${pairname}*dz_eul.tif` ; do
-    #rm ${workdir}/chm/${pairname}*dz_eul.tif
-	ln -sf $dz $(dirname ${workdir})/chm
+for dz in `ls ${pairname_dir}/${pairname}*dz_eul.tif` ; do
+	ln -sf $dz ${work_dir}/chm/$(basename ${dz})
 done
 
-#rm -v $workdir/out_*dz_eul*
-rm ${workdir}/*slope.tif
-#rm ${workdir}/*_masked.tif
-#rm -v $workdir/*.vrt
+rm ${pairname_dir}/out-*sr${search_rad_frmt}*masked.tif 2> /dev/null
+rm -rf /tmp/magick-* 2> /dev/null
 
-rm -rf /tmp/magick-*
+echo "----Done---- $(date)" ; echo
--- run_do_hrsi_chm_filt.sh
+++ run_do_hrsi_chm_filt.sh
@@ -7,34 +7,67 @@
 #
 
 script_name=do_hrsi_chm_filt.sh
-main_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/outASP_TEST/test2_do_dem_filt
+INPUT=${1:-'v1'}
 
-batch=${main_dir}/${1}
-SPARSE=${2}
+if [ "${INPUT}" = "v1" ] ; then
+    main_dir=/att/pubrepo/DEM/hrsi_dsm
+    work_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/chm_work/hrsi_chm_filt                   #outASP_TEST/test2_do_dem_filt
+elif [ "${INPUT}" = "v2" ] ; then
+    main_dir=/att/pubrepo/DEM/hrsi_dsm/v2
+    work_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/chm_work/hrsi_chm_sgm_filt
+elif [ "${INPUT}" = "test" ] ; then
+    main_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/outASP/veg_mode/SGM_5_999_0
+    work_dir=/att/gpfsfs/briskfs01/ppl/pmontesa/chm_work/hrsi_chm_sgm_filt
+else
+    echo "Incorrect INPUT dir. Exiting."
+    exit 1
+fi
+
+mkdir -p $work_dir
+
+cd $work_dir
+
+batch=${work_dir}/${2}
+FCOV=${3:-'dense'}
+
 # in lat/lon
-xmin=$3
-ymin=$4
-xmax=$5
-ymax=$6
+xmin=$4
+ymin=$5
+xmax=$6
+ymax=$7
 
 #Use these for now
 res_fine=1			# the fine pixel res for DEMs
 res_coarse=4		# the coarse pixel res for DEMs
 max_slope=10		# the max slope above which pixels will be masked out
 
-if [ "$SPARSE" = true ] ; then
-    echo; echo "Sparse forest filtering only." ; echo
+echo; echo "Resolutions for filtering: ${res_fine}m (fine for canopy), ${res_coarse}m (coarse for ground)" ; echo
+if [ "$FCOV" = "sparse" ] ; then
+    echo "Sparse forest cover filtering."
     search_rad_list="2"
 else
-    echo; echo "Dense forest filtering." ; echo
-    search_rad_list="2 5 10"
+    echo "Dense forest cover filtering."
+    search_rad_list="2 5"
 fi
 
 hostN=`/bin/hostname -s`
+echo "List name:" ; echo ${batch}_${hostN} ; echo
 
-mkdir -p ${main_dir}/logs
+num_parjobs=1
+if [[ "$hostN" == *"crane"* ]] ; then
+   num_parjobs=2
+fi
+
+mkdir -p ${work_dir}/logs
 
 while read -r pairname; do
+    echo ; echo "Pairname: $pairname"
+
+    if [ ! -d "${main_dir}/${pairname}"  ] ; then
+        echo "Not found in $main_dir" ; echo "Try moving pairname dir from NOBACKUP to PUBREPO" ; echo "Exiting." ; echo
+        exit 1
+    fi 
+    
 
     #from QGIS   lx      ly       rx	uy
     #          "439903 7174636 446392 7179954"
@@ -44,37 +77,71 @@ while read -r pairname; do
     # Ary Mas
     # 101.7943 72.3898 102.4882 72.5515
     #rm -r ${main_dir}/${pairname}
-    mkdir -p ${main_dir}/${pairname}
+
+    mkdir -p ${work_dir}/${pairname}
 
     if [ ! -z "$xmin" -a "$xmin" != " " ]; then
-        prj_utm=$(utm_proj_select.py /att/pubrepo/DEM/hrsi_dsm/${pairname}/out-DEM_1m.tif)
+        prj_utm=$(utm_proj_select.py ${main_dir}/${pairname}/out-DEM_1m.tif)
     
         # Create bbox shp for a specific run
-        cmd="ogr2ogr -clipdst ${xmin} ${ymin} ${xmax} ${ymax} -t_srs EPSG:4326 ${main_dir}/${pairname}/bbox_tmp.shp /att/briskfs/pmontesa/userfs02/arc/world_adm0.shp"
+        cmd="ogr2ogr -clipdst ${xmin} ${ymin} ${xmax} ${ymax} -t_srs EPSG:4326 ${work_dir}/${pairname}/bbox_tmp.shp /att/briskfs/pmontesa/userfs02/arc/world_adm0.shp"
         echo $cmd
         eval $cmd
 
         # Reproject bbox to utm of input DEM
-        cmd="ogr2ogr -t_srs \"${prj_utm}\" ${main_dir}/${pairname}/bbox_utm.shp ${main_dir}/${pairname}/bbox_tmp.shp"
+        cmd="ogr2ogr -t_srs \"${prj_utm}\" ${work_dir}/${pairname}/bbox_utm.shp ${work_dir}/${pairname}/bbox_tmp.shp"
         echo $cmd        
         eval $cmd
 
         # Run a function in my bash_profile to convert utm bbox to the format needed for -t_prjwin
         source ~/.bash_profile
-        p2d_extent=$(ogr_extent ${main_dir}/${pairname}/bbox_utm.shp)
+        p2d_extent=$(ogr_extent ${work_dir}/${pairname}/bbox_utm.shp)
         
         echo $p2d_extent
-        rm ${main_dir}/${pairname}/bbox*
+        rm ${work_dir}/${pairname}/bbox*
     else
         echo "No bbox specified."
         p2d_extent=""
         #p2d_extent="439903 7184954 446392 7174636"
     fi
 
+    cmd_list=''
+    
+    # Loop over sr list and fill a cmd_list
     for search_rad in ${search_rad_list} ; do
-        cmd="${script_name} ${pairname} ${res_fine} ${res_coarse} ${max_slope} ${search_rad} true true true \"$p2d_extent\"" 
-        echo; echo $cmd ; echo
-        eval $cmd | tee ${main_dir}/logs/${script_name%.*}_${hostN}_${pairname}_searchrad${search_rad}.log
+        out_dz=${work_dir}/${pairname}/${pairname}_sr0${search_rad}_${res_coarse}m-sr0${search_rad}-min_${res_fine}m-sr0${search_rad}-max_dz_eul.tif
+        echo; echo $out_dz ; echo
+        #if [ ! -e ${out_dz} ] ; then        
+
+            cmd=''
+            cmd="${script_name} ${pairname} ${res_fine} ${res_coarse} ${max_slope} ${search_rad} ${INPUT} true true \"$p2d_extent\"" 
+            echo $cmd
+        
+            #eval $cmd | tee ${work_dir}/logs/${script_name%.*}_${hostN}_${pairname}_searchrad${search_rad}.log
+            cmd_list+=\ \'$cmd\'
+        #fi
     done
+
+    # If cmd_list isnt empty, do slopemasking of DEM once, here
+    if [ ! -z "$cmd_list" ] ; then
+
+        echo; echo "Do the slopemasking for upcoming run(s)." ; echo
+        ln -sf ${main_dir}/${pairname}/out-DEM_4m.tif ${work_dir}/${pairname}/out-DEM_4m.tif 2> /dev/null
+        ln -sf ${main_dir}/${pairname}/out-DEM_24m.tif ${work_dir}/${pairname}/out-DEM_24m.tif 2> /dev/null
+        eval parallel -j 2 'slopemask_dem.py {} -max_slope ${max_slope}' ::: ${work_dir}/${pairname}/out-DEM_24m.tif ${work_dir}/${pairname}/out-DEM_4m.tif
+
+        echo ; echo "Run each set in parallel" ; echo
+        echo $cmd_list
+
+        # Not sure how to tee this to a logfile
+        # num jobs depends on VM
+
+        eval parallel -j $num_parjobs ::: $cmd_list
+    else
+        echo "CHMs exist for: $pairname"; echo
+    fi
+
+    # Remove the slope.tif, slopemasked.tif, and masked DEMs
+    rm ${work_dir}/${pairname}/*{slope,masked}.tif 2> /dev/null
 
 done < ${batch}_${hostN}
