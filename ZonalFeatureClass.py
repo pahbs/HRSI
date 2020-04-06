2b4401934e479e7105fbe5c42fd04f4f900e9f1f
mwooten3 <margaret.wooten@nasa.gov>
2020-04-06 16:18:50 -0400

    testing new Zonal stats

--- ZonalFeatureClass.py
+++ ZonalFeatureClass.py
@@ -86,15 +86,17 @@ class ZonalFeatureClass(object):
             if ptVal >= 0.99 or ptVal == None: # 0 = Data. 1 and None = NoData. some results might be float if within 2m of data. .99 cause some no data points were returning that
                 # Point not under NoData should be removed
                 layer.DeleteFeature(feature.GetFID())
+                ds.ExecuteSQL('REPACK ' + layer.GetName())
                 cnt+=1
                 continue # Do nothing else
                 
             layer.SetFeature(feature)
-            ds.ExecuteSQL('REPACK ' + layer.GetName())
             
-        del ds # Close the dataset
-        del layer
-        del feature
+            
+#        del ds # Close the dataset
+#        del layer
+#        del feature
+        ds = layer = feature = None
         
         print "{} features should have been removed".format(cnt)
         return tempCopy
