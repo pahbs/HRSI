ff65106bc96e2576bdd668fd17e517abbc5726ce
mwooten3 <margaret.wooten@nasa.gov>
2020-06-05 15:39:14 -0400

    minor edit

--- 3DSI_zonalStats.py
+++ 3DSI_zonalStats.py
@@ -452,6 +452,7 @@ def main(args):
     batchCsv = os.path.join(baseDir, '_timing', 
                     '{}_{}__timing.csv'.format(zonalType, stack.stackType()))
     os.system('mkdir -p {}'.format(os.path.dirname(batchCsv)))
+    
     if not os.path.isfile(batchCsv):
         with open(batchCsv, 'w') as bc:
             bc.write('stackName,n layers,n zonal features,node,minutes\n')
