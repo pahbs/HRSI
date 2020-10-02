f88eb38fccf1ff6b34aa717b418aa5f79b8e4830
mwooten3 <margaret.wooten@nasa.gov>
2020-10-02 12:24:10 -0400

    misc changes to glasCsvToGdb.py

--- glasCsvToGdb.py
+++ glasCsvToGdb.py
@@ -19,13 +19,14 @@ Final .gdb will be moved to: /att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS_naBore
 NOTES:
     Working with GPKG, because I think the FileGDB driver is finicky.
     If input is .gdb, will write to .gpkg then convert to .gdb
-    
+    Added overwrite step (7/30) so that we can pick up if we left off. Still testing though
     
 """
 import os
 import csv
 import argparse
 import datetime
+import platform
 
 #import pandas as pd
 from collections import OrderedDict
@@ -33,6 +34,20 @@ from collections import OrderedDict
 from osgeo import ogr# gdal, ogr, osr
 from FeatureClass import FeatureClass
 from SpatialHelper import SpatialHelper
+
+# Breaking up the creation into batches between the 10 crane nodes:
+# node --> (x, y) where the y is not inclusive (skip when count = y)
+countDict = {'crane101': '0,5000000',
+             'crane102': '5000000,10000000',
+             'crane103': '10000000,15000000',
+             'crane104': '15000000,20000000',
+             'crane105': '20000000,25000000',
+             'crane106': '25000000,30000000',
+             'crane107': '30000000,35000000',
+             'crane108': '35000000,40000000',
+             'crane109': '40000000,45000000',
+             'crane110': '45000000,50000000'             
+            }
 
 def addBufferToOutput(rowDict, layer, bufferDist):
     
@@ -123,6 +138,19 @@ def createFields(layer, columns):
 
     return None
 
+def rowDictToCsv(rowDict, writeCsv):
+    
+    # Write header if .csv does not exist;
+    if not os.path.isfile(writeCsv):
+         with open(writeCsv, 'w') as wc: 
+             wc.write('{}\n'.format(','.join(rowDict.keys())))
+             
+    # Write row
+    with open(writeCsv, 'a') as wc: 
+        wc.write('{}\n'.format(','.join(rowDict.values())))
+  
+    return None
+           
 def filterRow(rowDict):
     
     # Only use points if the following criteria is True
@@ -148,6 +176,10 @@ def main(args):
     outGdb     = args['outputGdb']
     bufferDist = args['bufferDistance']
     
+    overwrite = True # Set to False if we want to append to gdb/gpkg
+
+    node = platform.node()
+    
     """
     inCsv = '/att/gpfsfs/briskfs01/ppl/pmontesa/userfs02/data/glas/circ_boreal/gla01-boreal50up-fix2-data.csv'
     outGdb = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS/GLAS_boreal.gdb'
@@ -158,6 +190,12 @@ def main(args):
     hdrFile = '/att/nobackup/mwooten3/3DSI/GLAS/GLAS_csv_columns.txt'
     with open(hdrFile, 'r') as hf:
         hdr = hf.readline().strip('\n').split(',')
+        
+    # To save rows with bad lat/lon values to csv:
+    #badCoordCsv = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS/badCoords.csv'
+    
+    # To save info about batches (already made header in file)
+    batchCsv = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/GLAS/batchTrack.csv'
     
     # Hardcode the additional columns we want to add to .gdb
     # Doing uniqueID separate because we want it to be first
@@ -169,34 +207,50 @@ def main(args):
     
     # Get the output .gpkg because that's what we are writing to 
     if ext == '.gdb':
-        outGpkg = outGdb.replace('.gdb', '.gpkg')
+        outGpkg = outGdb.replace('.gdb', '__{}.gpkg'.format(node))
         convertToGdb = True
     elif ext == '.gpkg':
-        outGpkg = outGdb
+        outGpkg = outGdb.replace('.gpkg', '__{}.gpkg'.format(node))
         convertToGdb = False
+    convertToGdb = False #TEMP - set to False until last batch
     
-    print "\nCreating {}...".format(outGpkg)
-        
-    # Set up the output gdb
     driver = ogr.GetDriverByName('GPKG')
-    ds = driver.CreateDataSource(outGpkg)
-    srs = ogr.osr.SpatialReference()
-    srs.ImportFromEPSG(4326) # WGS84 Lat/Lon
     
-    # Create file with srs 4326; layerName = basename
-    layer = ds.CreateLayer(bname, srs, ogr.wkbPolygon)
-    
-    # Create the fields in the output .gdb using hdr contents and addl columns   
-    createFields(layer, hdr + extraColumns)
+    # Create the output if overwrite is True or file does not exist
+    if overwrite or not os.path.isfile(outGpkg):    
+        print "\nCreating {}...".format(outGpkg)
+                
+        # Set up the output gdb
+        ds = driver.CreateDataSource(outGpkg)
+        srs = ogr.osr.SpatialReference()
+        srs.ImportFromEPSG(4326) # WGS84 Lat/Lon
+        
+        # Create file with srs 4326; layerName = basename
+        layer = ds.CreateLayer(bname, srs, ogr.wkbPolygon)
+        
+        # Create the fields in the output .gdb using hdr contents and addl columns   
+        createFields(layer, hdr + extraColumns)
      
+    # Open the layer if not overwriting
+    else:
+        print "\nOpening and appending to {}...".format(outGpkg)
+        
+        ds = driver.Open(outGpkg)
+        layer = ds.GetLayer()
+        
+    # 8/14 Now that we are hardcoding batches for crane nodes, get that info
+    cntStart = int(countDict[node].split(',')[0])
+    cntEnd   = int(countDict[node].split(',')[1])
+        
     # Iterate through rows in .csv and write buffered polygons to output
     totCnt = 0 # count all of the points, not just the unfiltered ones
     badCnt = 0 # count of points with missing columns
     fltrCnt = 0 # count of points that were filtered
     cnt = 0 # count of points added to gdb
     
-    badLon = [] # list of longitudes that are < 180
-    
+    badLon = [] # list of longitudes that are < or > 180
+    #import pdb; pdb.set_trace()
+ 
     with open(inCsv, 'r') as c:
         
         data = csv.reader(c)
@@ -205,14 +259,16 @@ def main(args):
             
             totCnt += 1
             
-            if cnt < 1513600:
+            # split into batch depending on crane node
+            if totCnt < cntStart or totCnt >= cntEnd:
                 continue
 
             # Print count at every half millionth point (total not just unfiltered)
-            if totCnt % 500000 == 0:
+            if totCnt % 50000 == 0:  # count ever 5,000 row now
                 print " Total count so far = {}".format(totCnt)
                 print "  (Added count = {})".format(cnt)
-            
+                #layer.SyncToDisk() # Hopefully save info from memory to gpkg - dont think this worked            
+        
             # Sometimes a row has fewer columns than the header. Skip those
             if len(row) != len(hdr):
                 badCnt += 1
@@ -222,34 +278,51 @@ def main(args):
             rowDict = OrderedDict() # keep column order
             for c in hdr: rowDict[c] = row[hdr.index(c)]
 
-            # Some rows have a large longitude. Record info in list
-            if float(rowDict['lon']) > 180:
+            # Some rows have a too large/small longitude. Record info in list
+            if float(rowDict['lon']) > 180 or float(rowDict['lon']) < -180:            
                 badLon.append('{}: {}'.format(rowDict['lon'], totCnt))
+                rowDict['totCnt'] = str(totCnt)
+                #rowDictToCsv(rowDict, badCoordCsv) # save to .csv
                 continue # and skip
-            
+                
+            # Check for lat too just in case
+            if float(rowDict['lat']) > 90 or float(rowDict['lat']) < -90:
+                badLon.append('{}: {}'.format(rowDict['lat'], totCnt))
+                rowDict['totCnt'] = str(totCnt)
+                #rowDictToCsv(rowDict, badCoordCsv)                
+                continue # and skip
+                
             # Filter the row. Will return False if we want to keep the point
             if filterRow(rowDict):
                 fltrCnt += 1
                 continue # if True, skip point                
-            
-            cnt += 1 # only add to count if point is added
 
-            # Add some attributes to the dict:
-            rowDict['uniqueID']  = cnt # Add unique ID to dict
+            rowDict['uniqueID']  = cnt + 1 # Add unique ID to dict
             rowDict['bufferSize'] = bufferDist
             rowDict = addShotDate(rowDict)
             
             # Now send the row to function to buffer and add to output
             addBufferToOutput(rowDict, layer, bufferDist)     
 
+            cnt += 1 # only add to count if point is added
+                        
     nFeat1 = FeatureClass(outGpkg).nFeatures
+
+    # Try destroying the ds to free up memory and save:
+    ds.Destroy() 
 
     endTime1 = datetime.datetime.now()
     elapsedTime1 = endTime1 - startTime
     
-    print badLon
+    # Record info:
+    # node,start,end,totCnt,addCnt,nFeat,missingCols,badCoords,filtered, time (minutes)
+    rec = '{},{},{},{},{},{},{},{},{},{}\n'.format(node, cntStart, cntEnd, totCnt,
+                                   cnt, nFeat1, badCnt, len(badLon), fltrCnt, 
+                                   *divmod(elapsedTime1.total_seconds(), 60))
+    with open(batchCsv, 'a') as bc:
+        bc.write(rec)
     
-    print "\nFinished creating .gpkg ({} features) in ~{} minutes".              \
+    print "\nFinished creating .gpkg ({} features) in ~{} minutes".           \
                       format(nFeat1, *divmod(elapsedTime1.total_seconds(), 60))
                       
     print "\nRow counts:"
@@ -270,7 +343,7 @@ def main(args):
         endTime2 = datetime.datetime.now()
         elapsedTime2 = endTime2 - endTime1
         
-        print "\nFinished converting to .gdb ({} features) in ~{} minutes\n".  \
+        print "\nFinished converting to .gdb ({} features) in ~{} minutes\n". \
                       format(nFeat2, *divmod(elapsedTime2.total_seconds(), 60))
         
         return outGdb
