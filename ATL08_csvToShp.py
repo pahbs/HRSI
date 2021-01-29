7c7ff861eeed171e4f625d6903ab1e67c811eb04
mwooten3 <margaret.wooten@nasa.gov>
2021-01-29 14:27:07 -0500

    fixed calcgrounddirection error after filtering

--- ATL08_csvToShp.py
+++ ATL08_csvToShp.py
@@ -381,10 +381,10 @@ def main(args):
     # Log output:
     logFile = os.path.join(outLogDir, 'ATL08-csv_to_shp__{}__Log.txt'.format(bname))
     print "See {} for log".format(logFile)
-    #so = se = open(logFile, 'a', 0) # open our log file
-   # sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) # re-open stdout without buffering
-    #os.dup2(so.fileno(), sys.stdout.fileno()) # redirect stdout and stderr to the log file opened above
-   # os.dup2(se.fileno(), sys.stderr.fileno())
+    so = se = open(logFile, 'a', 0) # open our log file
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) # re-open stdout without buffering
+    os.dup2(so.fileno(), sys.stdout.fileno()) # redirect stdout and stderr to the log file opened above
+    os.dup2(se.fileno(), sys.stderr.fileno())
 
     print "BEGIN: {}".format(time.strftime("%m-%d-%y %I:%M:%S"))
     print "Continent: {}".format(cont)
@@ -418,11 +418,11 @@ def main(args):
     lonArr = np.asarray(pdf['lon']) 
     
     nInputRows = len(pdf)
-    import pdb; pdb.set_trace()
+
     # calculategrounddirection() fails if there is only one footprint in csv.
     # Skip if only one footprint:
     if len(latArr) <= 1:
-        print "\n CSV {} does not have at least two rows. Skipping".format(inCsv)
+        print "\n CSV {} has fewer than two rows. Skipping".format(inCsv)
         return None 
     
     # 2. Convert lat/lon lists to appropriate UTM zone
@@ -437,7 +437,7 @@ def main(args):
     
     # Cannot continue if at least two points do not exist
     if len(pdf) <= 1:
-        print "\n CSV {} does not have at least two rows after filtering. Skipping".format(inCsv)
+        print "\n CSV {} has fewer than rows after filtering. Skipping".format(inCsv)
         return None  
     
     # 5. Edit some columns
