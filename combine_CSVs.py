f83b790a04ad448589576f54bc5251781962831d
maggie w <margaret.wooten@nasa.gov>
2021-11-29 18:17:53 -0500

    edited combine_CSVs.py

--- combine_CSVs.py
+++ combine_CSVs.py
@@ -18,8 +18,6 @@ import os, sys
 import glob
 
 # Inputs:
-#inDir =  '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/ZonalStats/ATL08/Landsat/h*'
-#outCsv = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/ZonalStats/ATL08__Landsat__ZonalStats__rewrite.csv'
 inTxt = sys.argv[1]
 outCsv = sys.argv[2]
 outTxt = outCsv.replace('.csv', '__inputFiles.txt')
@@ -36,7 +34,7 @@ if os.path.isfile(outCsv): # if output file exists, we want to append to it
 with open(inTxt, 'r') as it:
     csvList = [f.strip() for f in it.readlines()]
    
-print "Combining {} .csv files...\n".format(len(csvList))
+print("Combining {} .csv files...\n".format(len(csvList)))
 
 first = True
 concatList = []
@@ -45,11 +43,11 @@ for c in csvList:
     #import pdb; pdb.set_trace()
 
     if not os.path.isfile(c):
-        print "{} does not exist. Skipping".format(c)
+        print("{} does not exist. Skipping".format(c))
         continue
     
     cnt += 1
-    print cnt
+    print(cnt)
 
     df = pd.read_csv(c)
 
@@ -63,7 +61,7 @@ for c in csvList:
     if hdr == list(df.columns):
         concatList.append(df)
     else:
-        print "CSV {} has a different header from {} and cannot be added to the output CSV".format(c, firstC)
+        print("CSV {} has a different header from {} and cannot be added to the output CSV".format(c, firstC))
         continue
 
     # If file exists and is being added to output .csv, write to out txt:
@@ -73,11 +71,12 @@ for c in csvList:
 #print concatList
 
 outdf = pd.concat(concatList, ignore_index=True).drop_duplicates().reset_index(drop=True)
-print "\n{} rows in final output df".format(len(outdf))
+print("\n{} rows in final output df".format(len(outdf)))
 
 ##print 'ya'
 outdf.to_csv(outCsv, index=False)
-print "Wrote to {}".format(outCsv)
+print("Wrote to {}".format(outCsv))
+
 ##    if first:
 ##        outdf = df
 ##        hdr = df.columns
